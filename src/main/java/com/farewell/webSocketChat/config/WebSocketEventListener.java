b5151c5d9d96a44d137725a4918c6b36a504e28e
ZealYou99 <awenez333@gmail.com>
2025-04-28 19:29:03 +0200

    Initial commit

---
+++ src/main/java/com/farewell/webSocketChat/config/WebSocketEventListener.java
@@ -0,0 +1,34 @@
+package com.farewell.webSocketChat.config;
+
+import com.farewell.webSocketChat.chat.ChatMessage;
+import com.farewell.webSocketChat.chat.MessageType;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+    private final SimpMessageSendingOperations messagingTemplate;
+    @EventListener
+    public void HandleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) accessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info("Disconnected from " + username);
+            var chatMessage = ChatMessage.builder()
+                    .type(MessageType.LEAVE)
+                    .sender(username)
+                    .build();
+            messagingTemplate.convertAndSend("/topic/public", chatMessage);
+        }
+
+    }
+}
